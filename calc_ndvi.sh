5ceb62955f28ade8dbc159b5d2a3538e8b7ef64d
Ubuntu User <ubuntu@packer-ubuntu-16.04-server-one>
2017-01-12 17:06:40 +0100

    Bash Script for calculating NDVI from LE* Image, resampling to 60 m resolution reprojecting to Lat/Long WGS84

---
+++ calc_ndvi.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "teamname: Kraftfahrzeug-Haftpflichtversicherung"
+echo "authors: Felten, Bettina; Stuhler, Sophie C."
+echo "date: 12-01-2017"
+echo "Assignment 4"
+echo "Calculate NDVI from Landsat 7 Image"
+echo "Resample NDVI Image to 60 m Resolution"
+
+fn=$(ls LE*.tif)
+echo "input file: $fn"
+ndvi="NDVI.tif"
+fntemp="NDVI60m.tif"
+fnout="NDVI60mLatLong.tif"
+
+echo "calculate Normalized Difference Vegetation Index"
+echo "NDVI is a ratio calculated from the difference between NIR and RED reflectance"
+echo "NIR: band 4; RED: band 3"
+gdal_calc.py -A $fn --A_band=4 -B $fn --B_band=3 --outfile=$ndvi --calc="(A.astype(float)-B)/(A.astype(float)+B)" --type='Float32'
+
+echo "resample to 60 m"
+echo "units are meters"
+gdal_translate -tr 60 60 $ndvi $fntemp
+
+echo "finally, reproject to Lat/Long WGS84"
+gdalwarp -t_srs EPSG:4326 $fntemp $fnout
